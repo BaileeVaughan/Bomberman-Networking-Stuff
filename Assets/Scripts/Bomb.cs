9c795bbd7c55712793b0318de9503cb00c3ae96b
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-08-28 13:14:21 +1000

    Initial commit

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public LayerMask levelMask;
+
+    private bool exploded = false;
+
+    void Start()
+    {
+        Invoke("Explode", 3f);
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        StartCoroutine(CreateExplosions(Vector3.forward));
+        StartCoroutine(CreateExplosions(Vector3.back));
+        StartCoroutine(CreateExplosions(Vector3.left));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        GetComponent<MeshRenderer>().enabled = false;
+        exploded = true;
+        transform.Find("Collider").gameObject.SetActive(false);
+        Destroy(gameObject, .3f);
+    }
+
+    private IEnumerator CreateExplosions(Vector3 dir)
+    {
+        for (int i = 1; i < 66; i++)
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position + new Vector3(0, .5f, 0), dir, out hit, 1, levelMask);
+            if (!hit.collider)
+            {
+                Instantiate(explosionPrefab, transform.position + (i * dir), explosionPrefab.transform.rotation);
+            }
+            else
+            {
+                break;
+            }
+        }
+        yield return new WaitForSeconds(.05f);
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (!exploded && col.CompareTag("Explosion"))
+        {
+            CancelInvoke("Explode");
+            Explode();
+        }
+    }
+}
